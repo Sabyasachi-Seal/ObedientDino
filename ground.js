f443ce4c36aca431dfeabd935cd99181ebaf8029
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-10-18 20:05:50 +0530

    Bugs

--- ground.js
+++ ground.js
@@ -1,20 +1,27 @@
-import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./updateCustomProperty.js"
+import {
+  getCustomProperty,
+  incrementCustomProperty,
+  setCustomProperty,
+} from "./updateCustomProperty.js";
 
-const SPEED = .05;
+const SPEED = 0.1;
+const groundElems = document.querySelectorAll("[data-ground]");
 
-const groundElems = document.querySelectorAll("[data-ground]")
-
-export function setupGround(){
-    setCustomProperty(groundElems[0], "--left", 0)
-    setCustomProperty(groundElems[1], "--left", 300)
+export function setupGround() {
+  console.log(getCustomProperty(groundElems[0], "--left"));
+  console.log(getCustomProperty(groundElems[1], "--left"));
+  setCustomProperty(groundElems[0], "--left", 0);
+  setCustomProperty(groundElems[1], "--left", 300);
+  console.log(getCustomProperty(groundElems[0], "--left"));
+  console.log(getCustomProperty(groundElems[1], "--left"));
 }
 
-export function updateGround(delta){
-    groundElems.forEach(ground => {
-        incrementCustomProperty(ground, "--left", delta * SPEED * -1)
+export function updateGround(delta) {
+  groundElems.forEach(ground => {
+    incrementCustomProperty(ground, "--left", delta * SPEED * -1);
 
-        if(getCustomProperty(ground, "--left") <= -300){
-            incrementCustomProperty(ground, "--left", 600);
-        }
-    })
+    if (getCustomProperty(ground, "--left") <= -300) {
+      incrementCustomProperty(ground, "--left", 600);
+    }
+  });
 }
--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-import { setupGround, updateGround } from './ground.js'
+import { setupGround, updateGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
 
-const worldElem = document.querySelector('[data-world]')
+const worldElem = document.querySelector("[data-world]");
 
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
 
 setupGround();
 
 let lastTime
-function update(time){
-    if(lastTime === null){
-        lastTime = time
-        window.requestAnimationFrame(update);
-        return;
-    }
-    const delta = time - lastTime;
-
-    updateGround(delta)
-
+function update(time) {
+  if (lastTime === null) {
     lastTime = time
-    window.requestAnimationFrame(update);
+    window.requestAnimationFrame(update)
+    return
+  }
+  const delta = time - lastTime
+
+  updateGround(delta)
+
+  lastTime = time
+  window.requestAnimationFrame(update)
 }
 window.requestAnimationFrame(update);
 
 function setPixelToWorldScale() {
-    let worldToPixelScale 
-    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-        worldToPixelScale = window.innerWidth / WORLD_WIDTH
-    }
-    else{
-        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
-    }
-    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
-}
+  let worldToPixelScale
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+  }
 
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
